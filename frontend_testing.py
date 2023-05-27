cbb8ca77bfa3694d7a9886f667b1483039a572b9
rotemlevin271 <80693540+rotemlevin271@users.noreply.github.com>
2023-05-27 20:41:09 +0300

    Update frontend_testing.py

--- frontend_testing.py
+++ frontend_testing.py
@@ -5,7 +5,7 @@ from time import sleep
 # starting a webdriver session
 driver = webdriver.Chrome(service=Service("C:/Users/rotem/Downloads/chromedriver_win32/chromedriver.exe"))
 # navigating to URL with existing id
-driver.get('https://127.0.0.1:5001/users/1')
+driver.get('http://127.0.0.1:5001/users/1')
 # check if username element showing and print it
 element = driver.find_elements(By.ID, "user")
 print(element)
