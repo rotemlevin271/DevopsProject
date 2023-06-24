998040f8fe42aa217425f76800ab5b1e71a8994e
rotemlevin271 <80693540+rotemlevin271@users.noreply.github.com>
2023-06-24 20:26:01 +0300

    Update clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -1,7 +1,8 @@
 import requests
 
 requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+# for the third part of the project we dont use the frontend
+# requests.get('http://127.0.0.1:5001/stop_server')
 
 try:
     response = requests.get('http://127.0.0.1:5000/stop_server')
@@ -10,9 +11,9 @@ try:
 except requests.RequestException as e:
     print(f"Error stopping server at http://127.0.0.1:5000: {str(e)}")
 
-try:
-    response = requests.get('http://127.0.0.1:5001/stop_server')
-    response.raise_for_status()
-    print("Server at http://127.0.0.1:5001 stopped successfully.")
-except requests.RequestException as e:
-    print(f"Error stopping server at http://127.0.0.1:5001: {str(e)}")
+# try:
+#     response = requests.get('http://127.0.0.1:5001/stop_server')
+#     response.raise_for_status()
+#     print("Server at http://127.0.0.1:5001 stopped successfully.")
+# except requests.RequestException as e:
+#     print(f"Error stopping server at http://127.0.0.1:5001: {str(e)}")
